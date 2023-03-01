69d70fff06ab30ed68db37d5203cd5647c80bda3
nicoleUY <106289210+nicoleUY@users.noreply.github.com>
2023-03-02 02:33:29 +0800

    transferring file to maze works
    problem lang is for some reason, can't read double digits properly, there is extra \n's
    also havent tried the actual bots yet
    ah also the inside of maze is diff, hindi na siya array and wla na yung rows and cols. 2d array list na siya and the rows and cols turned into 1 (size). pero i think i can try naman to make the array list into array ulit pero imma do it later

--- Maze.java
+++ Maze.java
@@ -1,51 +1,101 @@
+package ver2;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Maze {
 
-    private Coordinate[][] maze;
-    private int rows;
-    private int cols;
+//    private Coordinate[][] maze;
+    private ArrayList<ArrayList<Coordinate> > maze = new ArrayList<ArrayList<Coordinate>>();
+
+    private int size;
     private Coordinate entrance;
     private Coordinate exit;
 
-    public Maze(int rows, int cols) {
-        this.rows = rows;
-        this.cols = cols;
-
+    public Maze(int size) {
+        this.size = size;
+        this.maze = createMaze(size);
+        
+    }
+    
+    public ArrayList<ArrayList<Coordinate>> createMaze(int n){
         int i, j;
-        for(i = 0; i < this.rows; i++) {
-            for(j = 0; j < this.cols; j++) {
-                maze[i][j] = new Coordinate(i, j);
+        for(i = 0; i < n; i++) {
+        	maze.add(new ArrayList<Coordinate>());
+            for(j = 0; j < n; j++) {
+            	maze.get(i).add(new Coordinate(i,j));
+
             }
         }
+
+		return maze;
     }
 
-    public int getRows() {
-        return this.rows;
-    }
-
-    public int getCols() {
-        return this.cols;
-    }
-
-    public Coordinate getCoordinate(int x, int y) {
-        return this.maze[x][y];
-    }
-
-    public void setCoordinateData(int x, int y, String data) {
-        this.maze[x][y].setData(data);
-    }
-
+   
     public void findEntranceExit() {
         int i, j;
 
-        for(i = 0; i < this.rows; i++) {
-            for(j = 0; j < this.cols; j++) {
-                if(maze[0][i].getData() == "G") {
-                    this.entrance = this.maze[i][j];
-                } else if(maze[rows - 1][i].getData() == "S") {
-                    this.exit = this.maze[i][j];
+        for(i = 0; i < this.size; i++) {
+            for(j = 0; j < this.size; j++) {
+                if(maze.get(0).get(j).getData() == 'G') {
+                    this.entrance = this.maze.get(i).get(j);
+                } else if(maze.get(size-1).get(i).getData() == 'S') {
+                    this.exit = this.maze.get(i).get(j);
                 }
             }
         }
+    }
+    
+    public boolean isValidLocation(int x, int y) {
+        if((x <= (size - 1) && x >= 0) &&
+           (y <= (size - 1) && y >= 0) &&
+            this.maze.get(x).get(y).getData() == '.') {
+            
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean isExit(int x, int y) {
+        if(this.exit.getX() == x && this.exit.getY() == y) {
+            return true;
+        }
+        return false;
+    }
+    
+    public void printCoord(Coordinate c) {
+    	System.out.print(" ("+c.getX()+", "+c.getY()+") ");
+    }
+    
+    public void displayMaze() {
+    	int i=0, j=0;
+    	for(i = 0; i < this.size; i++) {
+            for(j = 0; j < this.size; j++) {
+                System.out.print(this.getCoordinate(i, j).getData());
+            }
+            System.out.println();
+        }
+    }
+    
+    public void dispMazeWCoord() {
+    	for(int i = 0; i < this.size; i++) {
+            for(int j = 0; j < this.size; j++) {
+                printCoord(this.getCoordinate(i, j));
+                System.out.print(": "+ this.getCoordinate(i, j).getData() + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    //GETTERS & SETTERS
+    
+    public Coordinate getCoordinate(int x, int y) {
+        return this.maze.get(x).get(y);
+    }
+
+    public void setCoordinateData(int x, int y, char data) {
+        this.maze.get(x).get(y).setData(data);
     }
 
     public Coordinate getEntrance() {
@@ -56,47 +106,38 @@ public class Maze {
         return this.exit;
     }
 
-    public boolean isValidLocation(int x, int y) {
-        if((x <= (rows - 1) && x >= 0) &&
-           (y <= (cols - 1) && y >= 0) &&
-            this.maze[x][y].getData() == ".") {
-            
-            return true;
-        }
-        return false;
+    
+    
+
+//    public int getRows() {
+//		return rows;
+//	}
+//
+//	public void setRows(int rows) {
+//		this.rows = rows;
+//	}
+//
+//	public int getCols() {
+//		return cols;
+//	}
+//
+//	public void setCols(int cols) {
+//		this.cols = cols;
+//	}
+
+    public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+    
+	public boolean isExplored(int x, int y) {
+        return maze.get(x).get(y).getIsVisited();
     }
 
-    public boolean isExit(int x, int y) {
-        if(this.exit.getX() == x && this.exit.getY() == y) {
-            return true;
-        }
-        return false;
-    }
-
-    public boolean isExplored(int x, int y) {
-        return maze[x][y].getIsVisited();
-    }
-
-    public void setExplored(int x, int y) {
-        maze[x][y].setIsVisited();
-    }
-
-    public void displayMaze() {
-
-        String[][] printableMaze = new String[this.rows][this.cols];
-        int i, j;
-
-        for(i = 0; i < this.rows; i++) {
-            for(j = 0; j < this.cols; j++) {
-                printableMaze[i][j] = this.maze[i][j].getData();
-            }
-        }
-
-        for(i = 0; i < this.rows; i++) {
-            System.out.println();
-            for(j = 0; j < this.cols; j++) {
-                System.out.print(printableMaze[i][j]);
-            }
-        }
+	public void setExplored(int x, int y) {
+    	maze.get(x).get(y).setIsVisited();
     }
 }
