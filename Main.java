528329e5358b641e99dc1232dfc1336d0782e630
nicoleUY <106289210+nicoleUY@users.noreply.github.com>
2023-03-03 17:25:26 +0800

    files work na

--- Main.java
+++ Main.java
@@ -1,69 +1,67 @@
-package mazeBot;
+package ver2;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
-//source: https://www.geeksforgeeks.org/read-file-into-an-array-in-java/
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		try {
-			// list that holds strings of a file
-	        List<String> listOfStrings
-	            = new ArrayList<String>();
-	       
-	        // load data from file
-	        FileReader fr = new FileReader("C:\\Users\\nicol\\OneDrive\\Desktop\\map.txt");
-	       
-	        // Created a string to store each character
-	        // to form word
-	        String s = new String();
-	        char ch;
-	       
-	        // checking for EOF
-	        while (fr.ready()) {
-	            ch = (char)fr.read();
-	               
-	            // Used to specify the delimiters
-	            if (ch == '\n') {
-	               
-	                // Storing each string in arraylist
-	                listOfStrings.add(s.toString());
-	               
-	                // clearing content in string
-	                s = new String();
-	            }
-	            else {
-	                // appending each character to string if the
-	                // current character is not delimiter
-	                s += ch;
-	            }
-	        }
-	        if (s.length() > 0) {
-	           
-	            // appending last line of strings to list
-	            listOfStrings.add(s.toString());
-	        }
-	        // storing the data in arraylist to array
-	        String[] array
-	            = listOfStrings.toArray(new String[0]);
-	       
-	        // printing each line of file which is stored in
-	        // array
-	        for (String str : array) {
-	            System.out.println(str);
-	        }
-		}
-		catch (IOException e) {
-			
-		}
-		
-		
+		int size;
+		size = mapSize("C:\\Users\\nicol\\OneDrive\\Desktop\\map.txt");
+		Maze m = new Maze(size);
+		fileToMaze("C:\\Users\\nicol\\OneDrive\\Desktop\\map.txt", m, size);
+		System.out.println(size);
+		if (m != null)
+			m.displayMaze();
 	}
 
+	public static int mapSize(String fileName) {
+		int n=0;
+		String s = "";
+		
+		try {
+    		//load data from file
+			
+		    BufferedReader br = new BufferedReader(new FileReader(fileName));
+		    s = br.readLine().trim();
+    		n = Integer.valueOf(s);    		
+	        br.close();
+    		
+    	} catch (IOException e) {
+    		System.out.println("An error occured");
+    		e.printStackTrace();}
+		
+		return n;
+	}
+	
+	//This function reads the text file and loads the data into the object Maze
+    public static void fileToMaze(String fileName, Maze m, int size) {
+    	    	
+    	String str = "";
+		int n;
+		
+		try {
+		    BufferedReader br = new BufferedReader(new FileReader(fileName));
+		    
+		    //getting size
+		    str = br.readLine().trim();
+    		n = Integer.valueOf(str);
+    		
+    		//setting char data per coordinate of map
+    		for (int i=0; i<n; i++) {
+    			str = br.readLine().trim();
+    			for (int j=0; j<n; j++) {
+    				m.setCoordinateData(i, j, str.charAt(j));
+    			}
+    		}
+    		
+	        br.close();
+    		
+    	} catch (IOException e) {
+    		System.out.println("An error occured");
+    		e.printStackTrace();}
+    }
 }
